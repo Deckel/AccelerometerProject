5764cc1ef35256c74112c25f880fb7651af6f88b
Deckel de Lange <deckel.delange@gmail.com>
2018-11-06 12:33:16 +0000

    Main program
    Added the main program

---
+++ rowingProject.py
@@ -0,0 +1,29 @@
+#A program that measures pitch and acceleration as a function of time
+
+import time
+
+timeStep = 0 
+#Open text file
+timestr = time.strftime("%H-%M-%S")
+print(timestr)
+F = open("data"+timestr+".txt","w")
+
+#Initial waiting time 60s
+time.sleep(1)
+print("Starting Recording")
+
+#Record for 10 mins at 10hz 
+while timeStep < 6:
+	time.sleep(0.1)
+	#Record Pitch
+	pitch = 12
+	#Record acceleration
+	acceleration = 5
+    #Print into 3 coloumns
+	F.write(str(round(timeStep,1))+"    ")
+	F.write(str(pitch)+"    ")
+	F.write(str(acceleration)+"\n")
+	#Incriment time
+	timeStep += 0.1
+#Closing the file
+F.close()
